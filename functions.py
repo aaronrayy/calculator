037f673a4355ceb7345cdd6f7769695a73986ddb
aaronrayy <aaronray.kruse@gmail.com>
2022-03-06 19:01:09 -0600

    added add, subtract, multiply, and divide functions

--- functions.py
+++ functions.py
@@ -4,3 +4,14 @@ def add(a, b):
     ret = a + b
     return ret
 
+def subtract(a, b):
+    ret = a - b
+    return ret
+
+def mult(a, b):
+    ret = a * b
+    return ret
+
+def div(a,b):
+    ret = a/b
+    return ret
